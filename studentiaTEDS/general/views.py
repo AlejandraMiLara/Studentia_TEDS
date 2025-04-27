e2f8c26bd937702b196282dbd852c69eeac573ab
AlejandraMiLara <a365544@uabc.edu.mx>
2025-04-27 14:18:03 -0700

    DESARROLLO: Historia -> {Iniciar Sesion}

--- studentiaTEDS/general/views.py
+++ studentiaTEDS/general/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+import datetime
+from django.contrib.auth import authenticate, login, logout, get_backends
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+import random
+import string
 
-# Create your views here.
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def iniciar_sesion(request):
+    msj = None
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            msj = "Correo o contraseña incorrectas. Intente de nuevo"
+    return render(request, 'iniciar_sesion.html', {'msj':msj})
