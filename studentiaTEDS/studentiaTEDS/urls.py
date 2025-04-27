1c7f88b6b732cbb594cc9b535ad696199a4ea09f
AlejandraMiLara <a365544@uabc.edu.mx>
2025-04-27 13:42:50 -0700

    Fix A: Setup Inicial

--- studentiaTEDS/studentiaTEDS/urls.py
+++ studentiaTEDS/studentiaTEDS/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for studentia project.
+URL configuration for studentiaTEDS project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
