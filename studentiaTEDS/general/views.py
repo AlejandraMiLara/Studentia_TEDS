1d0229ab39f6525ea1eb8ab7a18d9ebfa834dc1d
Yaeli833 <lourdesyaeli833@gmail.com>
2025-04-30 12:49:58 -0700

    Diseño:Historia->IniciarSesión,RegistrarUsuario,CerrarSesión

---
+++ studentiaTEDS/general/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+import datetime
+from django.contrib.auth import authenticate, login, logout, get_backends
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+import random
+import string
+from .forms import RegistroUsuarioForm
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def iniciar_sesion(request):
+    msj = None
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            msj = "Correo o contraseña incorrectas. Intente de nuevo"
+    return render(request, 'iniciar_sesion.html', {'msj':msj})
+
+def salir(request):
+    logout(request)
+    return redirect('inicio')
+
+def registrar_usuario(request):
+    if request.method == 'POST':
+        form = RegistroUsuarioForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+
+
+            backend = get_backends()[0] 
+            user.backend = f"{backend.__module__}.{backend.__class__.__name__}"
+
+            login(request, user) 
+
+            return redirect('inicio')
+    else:
+        form = RegistroUsuarioForm()
+
+    return render(request, 'registrar_usuario.html', {'form': form})
---
+++ studentiaTEDS/studentiaTEDS/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.contrib import admin
+from general import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.inicio, name='inicio'),
+    path('login/', views.iniciar_sesion, name="iniciar_sesion"),
+    path('logout/', views.salir, name='salir'),
+    path('signup/', views.registrar_usuario, name="registrar_usuario"),
+]
